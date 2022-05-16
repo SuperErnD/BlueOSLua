32cd1f91e7a99f497dc1f01940e21a3694f76e3f
SuperErnD <84639576+SuperErnD@users.noreply.github.com>
2022-05-16 10:42:08 +0300

    Add files via upload

--- setup.lua
+++ setup.lua
@@ -1,8 +1,32 @@
-if not http do 
+if not http then 
 	print("Enable HTTP-API to continue")
 	print("Installation cannot continue")
 	return
 end
 function downloadFile (url,file) 
+	setup = http.get(url)
 	
+	code = setup.readAll()
+	setup.close()
+	file=fs.open(file,'w')
+	file.write(code)
+	file.close()
 end
+io.write("Starting installation...")
+print("Downloading OS ")
+downloadFile('https://raw.githubusercontent.com/SuperErnD/BlueOS/main/rom.arch','rom.arch')
+print("Downloaded!")
+print("Downloading archive")
+downloadFile("https://raw.githubusercontent.com/SuperErnD/BlueOS/main/archive.lua",'archive.lua')
+print("Downloaded!")
+print("Unpacking OS")
+shell.execute("archive","rom.arch")
+print("Unpacked!")
+print('waiting for request')
+read()
+print("Readed!")
+print('Rebooting..')
+os.sleep(1)
+os.reboot()
+
+
